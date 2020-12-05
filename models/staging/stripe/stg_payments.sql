c3aedec3048dbbae78af8c63fde9361221c6be12
Todd Graham <tgraham@ucsc.edu>
2020-12-05 17:18:02 +0000

    troubleshooting config error with strip model

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,8 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-
-    -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
-    created as created_at
-
-from raw.stripe.payment 
+select id as payment_id, 
+       orderid as order_id, 
+       paymentmethod as payment_method, 
+       status, 
+       amount / 100 as amount,
+       created as created_dt
+from 
+"RAW"."STRIPE"."PAYMENT"
